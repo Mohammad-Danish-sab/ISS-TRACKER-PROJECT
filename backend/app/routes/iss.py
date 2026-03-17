84e1d1a18810a31c02a57dc91210ae91fe091f48
Mohammad Danish <danish.sab05@gmail.com>
2026-03-18 01:42:58 +0530

    fix the iss.py

--- backend/app/routes/iss.py
+++ backend/app/routes/iss.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
-from app.services.iss_service import get_iss_location
+from app.services.iss_service import get_iss_location, get_astronauts
 
 router = APIRouter()
 
 @router.get("/iss/location")
 def iss_location():
     return get_iss_location()
+
+@router.get("/iss/astronauts")
+def astronauts():
+    return get_astronauts()
+
