12f00a6f8ff03a4d0e7b24302eb2a00d47f29e49
Mohammad Danish <danish.sab05@gmail.com>
2026-03-17 14:10:32 +0530

    fix iss_service.py page

--- backend/app/services/iss_service.py
+++ backend/app/services/iss_service.py
@@ -17,3 +17,20 @@ def get_iss_location():
         return {
             "error": "ISS API is not responding right now"
         }
+    
+def get_astronauts():
+    url = "http://api.open-notify.org/astros.json"
+
+    try:
+        response = requests.get(url, timeout=5)
+        data = response.json()
+
+        return {
+            "number": data["number"],
+            "people": data["people"]
+        }
+
+    except requests.exceptions.RequestException:
+        return {
+            "error": "Astronaut API not available"
+        }
